6003b3bead3b899f1ed5b9bf24992fcd35409384
xaukael <37848032+xaukael@users.noreply.github.com>
2022-05-02 16:32:48 -0500

    Add files via upload

--- module.js
+++ module.js
@@ -2,9 +2,12 @@ Hooks.once('libWrapper.Ready', () => {
   libWrapper.register('inline-chats', 'TextEditor.enrichHTML', function(wrapped, ...args) {
 	const rgx = /\[\[!(.*?)\]\]/gi;
 	const matches = args[0].matchAll(rgx);
-	let icon = `<i class="far fa-comments"></i>`;
-	for (const match of matches) 
-  	args[0] = args[0].replace(match[0], `<a onclick="ui.chat.processMessage($(this).clone().children().remove().end().text());" class="inline-chat">${icon} ${match[0].replace(`[[!`,``).replace(`]]`,``).trim()}</a>`)
+	
+	for (const match of matches){
+		let text = match[0].replace(`[[!`,``).replace(`]]`,``).trim();
+		let icon = text.at(0)==='/'?'':'<i class="far fa-comments"></i>&nbsp;';
+  	args[0] = args[0].replace(match[0], `<a onclick="ui.chat.processMessage($(this).clone().children().remove().end().text());" class="inline-chat">${icon}${text}</a>`)
+	}
 	return wrapped(...args);
   }, 'WRAPPER');
 });
