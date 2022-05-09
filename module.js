9c223fbf58c2bc70c6f5b69be1598325245be1ca
xaukael <37848032+xaukael@users.noreply.github.com>
2022-05-08 20:30:50 -0500

    Add files via upload

--- module.js
+++ module.js
@@ -1,6 +1,7 @@
 Hooks.once('libWrapper.Ready', () => {
   libWrapper.register('inline-chats', 'TextEditor.enrichHTML', function(wrapped, ...args) {
 	const rgx = /\[\[!(.*?)\]\]/gi;
+	if (!args[0]) return wrapped(...args);
 	const matches = args[0].matchAll(rgx);
 	
 	for (const match of matches){
@@ -11,3 +12,9 @@ Hooks.once('libWrapper.Ready', () => {
 	return wrapped(...args);
   }, 'WRAPPER');
 });
+
+Hooks.on('renderChatMessage', (message, html)=>{
+  html.find('.inline-chat').click(function(){
+    ui.chat.processMessage($(this).clone().children().remove().end().text());
+  })
+})
