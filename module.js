4711652af0205d636c616a398db381749a7428c4
xaukael <37848032+xaukael@users.noreply.github.com>
2022-12-05 12:54:55 -0600

    Add files via upload

--- module.js
+++ module.js
@@ -1,20 +1,8 @@
-Hooks.once('libWrapper.Ready', () => {
-  libWrapper.register('inline-chats', 'TextEditor.enrichHTML', function(wrapped, ...args) {
-	const rgx = /\[\[!(.*?)\]\]/gi;
-	if (!args[0]) return wrapped(...args);
-	const matches = args[0].matchAll(rgx);
-	
-	for (const match of matches){
-		let text = match[0].replace(`[[!`,``).replace(`]]`,``).trim();
-		let icon = text.at(0)==='/'?'':'<i class="far fa-comments"></i>&nbsp;';
-  	args[0] = args[0].replace(match[0], `<a onclick="ui.chat.processMessage($(this).clone().children().remove().end().text());" class="inline-chat">${icon}${text}</a>`)
-	}
-	return wrapped(...args);
-  }, 'WRAPPER');
-});
 
-Hooks.on('renderChatMessage', (message, html)=>{
-  html.find('.inline-chat').click(function(){
-    ui.chat.processMessage($(this).clone().children().remove().end().text());
-  })
-})
+CONFIG.TextEditor.enrichers.push({pattern:/\[\[!(.*?)\]\]/gi, enricher: function enricher (match, options){
+    console.log("options",options);
+    let text = match[0].replace(`[[!`,``).replace(`]]`,``).trim();
+    let icon = text.at(0)==='/'?'':'<i class="far fa-comments"></i>&nbsp;';
+  	let element = $(`<a onclick="ui.chat.processMessage($(this).clone().children().remove().end().text().replace('{{','[[').replace('}}',']]'));" class="inline-chat">${icon}${text}</a>`)[0];
+    return element;
+}})
