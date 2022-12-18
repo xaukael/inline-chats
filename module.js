6f81862585f877958d54f2d824e3811b3c7ae175
xaukael <37848032+xaukael@users.noreply.github.com>
2022-12-18 05:15:03 -0600

    Add files via upload

--- module.js
+++ module.js
@@ -1,14 +1,31 @@
+TextEditor.onClickInlineChat = function(element, event, relativeToUuid){
+  let content = $(element).clone().children().remove().end().text().replace('{{','[[').replace('}}',']]');
+  if (content.at(0)==='/') return ui.chat.processMessage(content);
+  if (relativeToUuid == 'undefined') return ui.chat.processMessage(content);
+  let document;
+  if (game.release?.generation >= 10) document = fromUuidSync(relativeToUuid);
+  else document = CONFIG[data.type].collection.instance.get(relativeToUuid);
+  
+  console.log(document)
+  if (!document) return ui.chat.processMessage(content);
+  if (document.documentName == "Actor") 
+      return ChatMessage.create({content, speaker:ChatMessage.getSpeaker({actor: document})});
+  if (document.documentName == "JournalEntryPage")
+    return ChatMessage.create({content, speaker:ChatMessage.getSpeaker({alias: document.name})});
+  if (document.documentName == "Item")
+    if (document.parent.documentName == "Actor") return ChatMessage.create({content, speaker:ChatMessage.getSpeaker({actor: document.parent})});
+    else return ChatMessage.create({content, speaker:ChatMessage.getSpeaker({alias: document.name})});
+  return ui.chat.processMessage(content);
+
+}
+
 
 CONFIG.TextEditor.enrichers.push({pattern:/\[\[!(.*?)\]\]/gi, enricher: function enricher (match, options){
     console.log("options", options);
-    let id = options.relativeTo?.id;
+    let relativeToUuid = options.relativeTo?.uuid;
     let text = match[0].replace(`[[!`,``).replace(`]]`,``).trim();
-    let icon = text.at(0)==='/'?'':'<i class="far fa-comments"></i>&nbsp;';
-    if (options.relativeTo?.documentName == "Actor") 
-      return $(`<a onclick="ChatMessage.create({content:$(this).clone().children().remove().end().text().replace('{{','[[').replace('}}',']]'), speaker:ChatMessage.getSpeaker({actor: game.actors.get('${id}')})});" class="inline-chat">${icon}${text}</a>`)[0]
-    if (options.relativeTo?.documentName == "JournalEntryPage")
-      return $(`<a onclick="ChatMessage.create({content:$(this).clone().children().remove().end().text().replace('{{','[[').replace('}}',']]'), speaker:ChatMessage.getSpeaker({alias: '${options.relativeTo?.name}'})});" class="inline-chat">${icon}${text}</a>`)[0]
-    return $(`<a onclick="ui.chat.processMessage($(this).clone().children().remove().end().text().replace('{{','[[').replace('}}',']]'));" class="inline-chat">${icon}${text}</a>`)[0]
+    let icon = text.at(0)==='/'?'':'<i class="far fa-comment"></i>&nbsp;';
+    return $(`<a onclick="TextEditor.onClickInlineChat(this, event, '${relativeToUuid}')" class="inline-chat">${icon}${text}</a>`)[0]
 }})
 
 Hooks.on('renderChatMessage', (message, html)=>{
